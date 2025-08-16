950486ad6b6becceedaf9e7f4f0bb98750c15701
Kauã Silva <kaua.ribeiro@ufu.br>
2025-08-16 02:03:48 -0300

    Adicionando sprite e animação

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
@@ -22,4 +24,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if is_on_floor():
+		if direction > 0:
+			anim.flip_h = false
+			anim.play("walk")
+		elif direction < 0:
+			anim.flip_h = true
+			anim.play("walk")
+		else:
+			anim.play("idle")
+	else:
+		anim.play("jump")
+	
 	move_and_slide()
